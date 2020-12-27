636e0ec6fd51c3910f1f85cc5653150e87dbefc5
Haoting Qiu <easonyau@126.com>
2020-12-27 17:29:05 -0500

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by 邱浩庭 on 27/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
